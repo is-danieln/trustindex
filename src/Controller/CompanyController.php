515189efa323c3a8e3a793b30516a0000f902cf7
G <danieln@inlinestudio.hu>
2026-08-14 17:39:00 +0200

    Add company rating statistics

---
+++ src/Controller/CompanyController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Repository\ReviewRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CompanyController extends AbstractController
+{
+    #[Route('/companies', name: 'app_company_index', methods: ['GET'])]
+    public function index(Request $request, ReviewRepository $reviewRepository): Response
+    {
+        $query = trim((string) $request->query->get('q', ''));
+
+        return $this->render('company/index.html.twig', [
+            'companies' => $reviewRepository->findCompanyStatistics($query),
+            'query' => $query,
+        ]);
+    }
+}
